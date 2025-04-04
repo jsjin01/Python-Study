f57640c028bb5325f0df4c3f375f347362b7360c
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2025-04-05 02:07:33 +0900

    보너스 문제 풀기

--- MJsec_Python_Stady/Mjsec_Python/Week01/week01_04.py
+++ MJsec_Python_Stady/Mjsec_Python/Week01/week01_04.py
@@ -2,34 +2,57 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-dq = deque()
-dq.reverse()
-case_num = int(input().rstrip())
+input_num = int(input().rstrip())
 
-for i in range(case_num):
+for i in range(input_num):
+
+    dq = deque()
+    is_reverse = False
+    errer = False
+
     case_part =  input().rstrip()
-    input_num = int(input().rstrip())
-    input_text =  input().rstrip()
-    input_text.removeprefix("[")
-    input_text.removesuffix("]")
-    input_array : list = input_text.split(",")
-    for a in input_array:
-        dq.append(a)
+    case_num = int(input().rstrip())
+    case_temp= list(input().rstrip()[1:-1].split(","))
+    case_array = list()
+
+# 리스트 받아서 []없애기
+    if not case_num == 0:
+        for i in range(0, case_num):
+            case_array.append(case_temp[i])
+    
+    for l in case_array:
+        dq.append(l)
 
     for c in case_part:
-        if c == "R":
-            if not len(dq) == 0:
-                dq.reverse()
-            else:
-                print("errer")
-                break
+        if c == "R" :
+            is_reverse = not is_reverse
         elif c == "D":
-            if not len(dq) == 0:
-                dq.popleft()
+            if not len(dq) == 0 :
+                if is_reverse == False:
+                    dq.popleft()
+                else:
+                    dq.pop()
             else:
-                print("errer")
+                print("error")
+                errer = True
                 break
+        
+    s = ""
+    if not len(dq) == 0:
+        if is_reverse :
+            dq.reverse()
+        s += "["
+        for c in dq:
+            s += c +","
+        
+        s = s[:-1]
+        s += "]"
+        print(s)
+    elif not errer:
+        print("[]")
+    
+# 조건 잘 정리해서 코딩하기
 
 
-    if not len(dq) == 0: 
-        print(dq)
+
+
