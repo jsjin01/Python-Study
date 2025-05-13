f632c674a1f7e72a8c2d5e9b0184d760de85711f
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2025-05-14 00:41:25 +0900

    .

---
+++ MJsec_Python_Stady/Mjsec_Python/Week04/Problem1920.py
@@ -0,0 +1,23 @@
+import sys
+from bisect import bisect_left
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+N_array = list(map(int, input().rstrip().split()))
+
+M = int(input().rstrip())
+M_array = list(map(int, input().rstrip().split()))
+
+N_array.sort()
+
+for i in M_array:
+    if bisect_left(N_array,i) >= N:
+        print("0")
+    else:
+        if i == N_array[bisect_left(N_array,i)] :
+            print("1")
+        else:
+            print("0")
+
+##풀려있음
--- MJsec_Python_Stady/Mjsec_Python/Week04/Problem2110.py
+++ MJsec_Python_Stady/Mjsec_Python/Week04/Problem2110.py
@@ -8,11 +8,27 @@ N, C = map(int,input().rstrip().split())
 #리스트 컴프리헨션 => 집의 x_pos 잡기
 house_positon = [int(input().rstrip()) for _ in range(N)]
 
-# 1 2 4 8 9
 house_positon.sort()
 
-# key point => 일정 거리 이상이 되면 설치를 해야됨 
-# 그러면 기준이 되는 지점이 어떻게 나오는지 생각해봐야함
-# 기준은 두점 사이의 최대값보다는 작고 최솟값보다는 큰 값이여야함 
-# 총 거리를 C로 나눈 값보다 큰 수 중에서 가장 작은 좌표의 값을 설정
+max_d = house_positon[-1] - house_positon [0]
+min_d = 1
+length = 0
+while min_d <= max_d :
+    mid = (max_d + min_d) // 2
 
+    #거리에 따른 공유기 설치 가능 갯수 구하기 
+    cnt = 1
+    last_pos = house_positon[0]
+    for i in range (1, N):
+        if house_positon[i] - last_pos >= mid:
+            cnt += 1
+            last_pos = house_positon[i]
+    
+    if cnt >= C:
+        length = mid
+        min_d = mid + 1
+    else:
+        max_d = mid - 1
+
+
+print(length)
